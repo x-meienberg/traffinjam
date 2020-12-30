eee812bd006cb61ab38e71616621bd029428267a
Xeno Meienberg <xenomeienberg@gmx.ch>
2020-12-30 11:29:45 +0100

    Commit to 'personal branch'

--- access_alpaca.py
+++ access_alpaca.py
@@ -1,7 +1,10 @@
-import json
-import requests
+# This program controls the access to the alpaca paper trading API
+# The rate limit on trading is set to 200 requests per minute per API key
+#If HTTP status code 429, then rate limit is exceeded
+
 import config
 import alpaca_trade_api as tradeapi
+
 
 CLIENT_ID = config.client_id
 CLIENT_Secret = config.client_secret
@@ -16,3 +19,12 @@ api = tradeapi.REST(CLIENT_ID, CLIENT_Secret, BASE_URL, api_version='v2')
 account = api.get_account()
 
 print(account)
+
+positions = api.list_positions()
+print('Open Positions:')
+print(positions)
+
+#You can access status variables easily via the variable command
+status = account.status
+
+print(status)
