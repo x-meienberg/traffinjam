e20ab328180ed4aaa285f0be1ef30cc3af2412a3
Xeno Meienberg <xenomeienberg@gmx.ch>
2021-05-29 11:24:39 +0200

    Add new docs and rename trading strategy files

---
+++ trading_strategies.py
@@ -0,0 +1,15 @@
+# This is the part of the program where a trading strategy can be implemented
+
+
+#Equal weight strategy
+def trading_strategy_1():
+
+
+#Quantitattive momentum strategy
+def trading_strategy_2():
+
+
+#Quantitive value strategy
+def trading_strategy_3():	
+
+	
--- trading_strategy_1.py
+++
@@ -1,3 +0,0 @@
-# This is the part of the program where a trading strategy can be implemented
-
-def trading_strategy_1():
