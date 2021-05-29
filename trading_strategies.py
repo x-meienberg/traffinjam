a23efb2482f6ba90f71916f045a081574261db6d
Xeno Meienberg <xenomeienberg@gmx.ch>
2021-05-29 20:25:04 +0200

    Add new feature of relative returns to repo

--- trading_strategies.py
+++ trading_strategies.py
@@ -34,7 +34,19 @@ def trading_strategy_1(data, name_of_stock, days, ma_interval):
 		r_relative.append((mean_price[i]-mean_price[i-1]) / mean_price[i-1]) #calculate the relative returns
 		r_log.append(np.log(mean_price[i-1]/mean_price[i])) #calculate the log-returns
 	
-	# !!!! Do not Forget to Add a Section here for the cumulative log-returns !!!! -> c_cum
+	# Define cumulative sum c_cum of all log-returns in percentages [%]
+	c_cum.append(r_log[0])
+		
+	for i in range(1,horizon): 
+
+		c_cum.append(c_cum[i-1]+r_log[i])
+
+		
+	#Define all relative, cumulative returns in percentages [%] 
+	c_rel_percentage = 100*(np.exp(c_cum)-1)
+
+	#Redefine c_cum in percentages
+	c_cum_percentage = [element * 100 for element in c_cum]
 
 	#create plots
 	fig, ax = plt.subplots()
@@ -42,8 +54,10 @@ def trading_strategy_1(data, name_of_stock, days, ma_interval):
 	#plot each relevant time series
 	ax.plot(t[ma_interval:], r_relative[ma_interval:])
 	ax.plot(t[ma_interval:], r_log[ma_interval:])
+	ax.plot(t[ma_interval:], c_cum_percentage[ma_interval:])
+	ax.plot(t[ma_interval:], c_rel_percentage[ma_interval:])
 
-	labels = ["Relative Return", "Log-Returns"]
+	labels = ["Relative Return", "Log-Returns", "Cumulative Log-Returns", "Cumulative Relative Returns"]
 	ax.set(xlabel='Date', ylabel = 'Price [USD]')
 	ax.legend(labels)
 
@@ -51,7 +65,7 @@ def trading_strategy_1(data, name_of_stock, days, ma_interval):
 	ax.grid()
 	name_of_stock = str(name_of_stock)
 	days = str(days)
-	ax.set_title('Relative Returs of '+name_of_stock+ 'over last' + days + ' days')
+	ax.set_title('Relative Returns of '+name_of_stock+ 'over last' + days + ' days')
 	fig.autofmt_xdate()
 	fig.savefig('results/' + name_of_stock + '_relativeReturn_stock.svg')
 
